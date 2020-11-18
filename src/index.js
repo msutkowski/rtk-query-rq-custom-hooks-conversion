383c08ab04ff7aa4671d2d8bf2984013bedb7063
Matt Sutkowski <msutkowski@gmail.com>
2020-11-17 21:03:14 -0800

    Init from base

---
+++ src/index.js
@@ -0,0 +1,109 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  useQueryCache,
+  QueryCache,
+  ReactQueryCacheProvider,
+} from "react-query";
+import { ReactQueryDevtools } from "react-query-devtools";
+
+import usePosts from "./hooks/usePosts";
+import usePost from "./hooks/usePost";
+
+const queryCache = new QueryCache();
+
+function App() {
+  const [postId, setPostId] = React.useState(-1);
+
+  return (
+    <ReactQueryCacheProvider queryCache={queryCache}>
+      <p>
+        This example is exactly the same as the basic example, but each query
+        has been refactored to be it's own custom hook. This design is the
+        suggested way to use React Query, as it makes it much easier to manage
+        query keys and shared query logic.
+      </p>
+      {postId > -1 ? (
+        <Post postId={postId} setPostId={setPostId} />
+      ) : (
+        <Posts setPostId={setPostId} />
+      )}
+      <ReactQueryDevtools initialIsOpen />
+    </ReactQueryCacheProvider>
+  );
+}
+
+function Posts({ setPostId }) {
+  const cache = useQueryCache();
+  const { status, data, error, isFetching } = usePosts();
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <div>
+        {status === "loading" ? (
+          "Loading..."
+        ) : status === "error" ? (
+          <span>Error: {error.message}</span>
+        ) : (
+          <>
+            <div>
+              {data.map((post) => (
+                <p key={post.id}>
+                  <a
+                    onClick={() => setPostId(post.id)}
+                    href="#"
+                    style={
+                      // We can use the queryCache here to show bold links for
+                      // ones that are cached
+                      cache.getQueryData(["post", post.id])
+                        ? {
+                            fontWeight: "bold",
+                            color: "green",
+                          }
+                        : {}
+                    }
+                  >
+                    {post.title}
+                  </a>
+                </p>
+              ))}
+            </div>
+            <div>{isFetching ? "Background Updating..." : " "}</div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+function Post({ postId, setPostId }) {
+  const { status, data, error, isFetching } = usePost(postId);
+
+  return (
+    <div>
+      <div>
+        <a onClick={() => setPostId(-1)} href="#">
+          Back
+        </a>
+      </div>
+      {!postId || status === "loading" ? (
+        "Loading..."
+      ) : status === "error" ? (
+        <span>Error: {error.message}</span>
+      ) : (
+        <>
+          <h1>{data.title}</h1>
+          <div>
+            <p>{data.body}</p>
+          </div>
+          <div>{isFetching ? "Background Updating..." : " "}</div>
+        </>
+      )}
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
